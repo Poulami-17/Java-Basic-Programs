d7c54405ab5d85c6a4bf324e00b23be8dec828ff
Poulami Bera <65413328+Poulami-17@users.noreply.github.com>
2022-11-12 21:22:21 +0530

    Hollow Rectangle Pattern

---
+++ Hollow_Rectangle.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Hollow_Rectangle {
+    public static void PrintPattern(int row, int col) {
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (i == 1 || j == 1 || i == row || j == col)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        sc.close();
+        PrintPattern(row, col);
+    }
+}
