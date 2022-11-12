6160b0ea84999e46e88ddb60adfb7cc778583b48
Poulami Bera <65413328+Poulami-17@users.noreply.github.com>
2022-11-12 21:21:07 +0530

    Solid Rectangle Pattern

---
+++ Solid_Rectangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solid_Rectangle {
+    public static void PrintPattern(int row, int col) {
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++)
+                System.out.print("*");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+        int col = sc.nextInt();
+        sc.close();
+        PrintPattern(row, col);
+    }
+}
