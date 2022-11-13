adfa697aaa06436ee8b0ca540d4aade7f1309d88
Poulami Bera <65413328+Poulami-17@users.noreply.github.com>
2022-11-13 13:43:39 +0530

    Floyd's Triangle Pattern

---
+++ Floyd_Triangle.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Floyd_Triangle{
+    public static void PrintPattern(int n){
+        int num=1;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(); // Here, row number is same as column numnber
+        sc.close();
+        PrintPattern(n);
+    }
+}
