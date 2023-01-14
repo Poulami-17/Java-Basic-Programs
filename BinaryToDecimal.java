90d1d5ed43f2d1628088652cce115a3adbc52ddc
Poulami Bera <65413328+Poulami-17@users.noreply.github.com>
2023-01-14 15:20:50 +0530

    Binary To Decimal

---
+++ BinaryToDecimal.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class BinaryToDecimal {
+    public static int Convertion(int num){
+        int Dec=0,power=0;
+        while(num!=0){
+            int ld=num%10;
+            Dec=Dec+ld*(int)(Math.pow(2,power));
+            num=num/10;
+            power++;
+        }
+        return Dec;
+    }
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        System.out.println(Convertion(num));    
+        sc.close();
+    }
+}
